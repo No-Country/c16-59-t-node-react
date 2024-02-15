c0cacfdc49f00d08785739721bac11d338ac7517
WSmithDR <wagnersmith123@hotmail.com>
2024-02-14 22:18:44 -0500

    aprendiendo a usar next.js

---
+++ front/src/app/(site)/catalog/components/Vegetables.component.tsx
@@ -0,0 +1,7 @@
+export const Vegetables = async () => {
+    return(
+        <div>
+            Vegetables
+        </div>
+    )
+}
--- front/src/app/components/Menu.component.tsx
+++ front/src/app/components/Menu.component.tsx
@@ -1,12 +1,17 @@
+"use client"
+
 import { Button } from "@nextui-org/react";
+import { useRouter } from 'next/navigation';
 
 export const Menu = () => {
+  const router = useRouter()
+
   return (
     <div className="w-[90vw] lg:w-[70vw] h-[200px] bg-secundario flex flex-col gap-2 justify-center items-center">
       <Button className="btn-menu bg-terciario rounded-none font-bold">
         La Huerta <span className="italic font-medium">Box</span>
       </Button>
-      <Button className="btn-menu bg-terciario rounded-none font-bold">
+      <Button onClick={()=>router.push("/catalog")} className="btn-menu bg-terciario rounded-none font-bold">
         Productos del Mes
       </Button>
       <Button className="btn-menu bg-principal rounded-none font-bold">
