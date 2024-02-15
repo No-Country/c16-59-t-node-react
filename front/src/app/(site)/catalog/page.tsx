b4b5a8be2389044b9e93c3cfb25aae0e0ba1699d
Luis Angel <lguzman.58erb@outlook.com>
2024-02-15 13:33:36 -0500

    cambio

--- front/src/app/(site)/catalog/page.tsx
+++ front/src/app/(site)/catalog/page.tsx
@@ -3,7 +3,12 @@ import { NavBar } from "@/app/components";
 import { AccordionCatalog } from "./components";
 
 export default async function CatalogPage() {
-  const products = await getProdCatalog();
+  // const products = await getProdCatalog();
+
+  // aqui haz las peticiones para frutas, vegetales y productos envasados
+  // const frutas = await getFruitCatalog();
+  // const vegetales = await getVegeCatalog();
+  // const productsEnvasados = await getProdComesCatalog();
 
   return (
     <div>
