24b79d5c2c11f1764235c1c73666b974378ee545
Marco A. R. del A <marda_da-capo@hotmail.com>
2024-02-16 17:20:08 -0600

    feat: back-end config

---
+++ server/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    //To stablish the enviroment variables
+    ConfigModule.forRoot(),
+
+    //DB Config:
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, // False in production
+    }),
+  ],
+})
+export class AppModule {}
