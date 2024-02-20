e92951e63dce1e16841af1ba68146ee306993190
WSmithDR <wagnersmith123@hotmail.com>
2024-02-20 11:46:30 -0500

    corregido el problema de las rutas equivocadas

--- front/src/app/(site)/products/page.tsx
+++ front/src/app/(site)/products/page.tsx
@@ -1,7 +1,40 @@
-export default function Products() {
+import { AccordionCatalog } from "./components";
+import { Title } from "../components";
+import { getFruitCatalog, getVegetablesCatalog, getProcessedFoodsCatalog } from "@/utils/fetchApi";
+import { ButtonStandard, Footer } from "@/app/components";
+
+
+export default async function Subscriptions() {
+
+  // aqui haz las peticiones para frutas, vegetales y productos envasados
+  const fruits: any = await getFruitCatalog();
+   const vegetables: any = await getVegetablesCatalog();
+   const processedFoods: any = await getProcessedFoodsCatalog();
+  // const productsEnvasados = await getProdComesCatalog();
+  console.log("frutas donde estas?", fruits);
+  console.log("vegetales donde estas?", vegetables);
+
   return (
-    <div>
-      <h1>Products</h1>
-    </div>
+    <>
+      <Title>La Huerta Box - EXPRESS</Title>
+      <div className="flex flex-col  ">
+      <strong>1. Escoge los productos que deseas clickeando sobre cada ítem:  </strong>
+      <AccordionCatalog
+        vegetables={vegetables}
+        fruits={fruits}
+        processedFoods={processedFoods}
+      />
+      <div className="flex justify-around w-80">
+      <ButtonStandard secondary route="/" >
+        Anterior
+      </ButtonStandard>
+      <ButtonStandard primary route="/" >
+        Siguiente
+      </ButtonStandard>
+      </div>
+      </div>
+      <Footer/>
+    </>
+
   );
 }
