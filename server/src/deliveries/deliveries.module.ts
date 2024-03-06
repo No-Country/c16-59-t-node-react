29633cf360b00bbaa815e195eb1c0bbe13db65e4
Marco A. R. del A <marda_da-capo@hotmail.com>
2024-03-05 20:38:40 -0600

    fix: import authModule on deliveriesModule to solve problems in prod

--- server/src/deliveries/deliveries.module.ts
+++ server/src/deliveries/deliveries.module.ts
@@ -3,11 +3,12 @@ import { DeliveriesService } from './deliveries.service';
 import { DeliveriesController } from './deliveries.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Delivery } from './entities/delivery.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [DeliveriesController],
   providers: [DeliveriesService],
-  imports: [TypeOrmModule.forFeature([Delivery])],
+  imports: [AuthModule, TypeOrmModule.forFeature([Delivery])],
   exports: [TypeOrmModule],
 })
 export class DeliveriesModule {}
