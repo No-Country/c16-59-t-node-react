2ee339b148d02e867b90c3505bcb42e4ca68c30d
lags2022 <lguzman.58erb@outlook.com>
2024-03-07 17:47:51 -0500

    feat: arreglando 9

--- front/src/app/(site)/products/delivery-options/resume/page.tsx
+++ front/src/app/(site)/products/delivery-options/resume/page.tsx
@@ -21,7 +21,7 @@ export default function Resume() {
         La Huerta Box - EXPRESS
       </Title>
 
-      <div className="max-w-5xl mx-auto space-y-4">
+      {/* <div className="max-w-5xl mx-auto space-y-4">
         <ElementResume title="Datos de Facturación">
           <FormResume />
         </ElementResume>
@@ -60,7 +60,7 @@ export default function Resume() {
         <ElementResume title="Formas de pago">
           <ButtonsGroupPayment />
         </ElementResume>
-      </div>
+      </div> */}
       <div className="w-full flex justify-center items-center m-auto">
         <RouteBtn
           size="lg"
