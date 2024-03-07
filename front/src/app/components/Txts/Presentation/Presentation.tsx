54f10c9dd5fd178a80729d2e533829375f0ddf2c
WSmithDR <wagnersmith123@hotmail.com>
2024-03-07 15:42:32 -0500

    menudo quilombo

--- front/src/app/components/Txts/Presentation/Presentation.tsx
+++ front/src/app/components/Txts/Presentation/Presentation.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { presentationClass } from './classNames';
 
 interface PresentationProps {
-  price: Price;
+  price?: Price;
 }
 
 export const Presentation: React.FC<PresentationProps> = ({ price }) => (
-  price !== "" ?
+  price !== undefined ?
     <span className={presentationClass}>Contiene*:</span>
     :
     <span className={presentationClass}>Puedes escoger las <strong>opciones y cantidades</strong> que desees de:</span>
