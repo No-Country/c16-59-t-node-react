5a20c50070324148e488a1d2f07f7329d3579ef8
WSmithDR <wagnersmith123@hotmail.com>
2024-03-07 21:14:01 -0500

    boludo

--- front/src/app/(site)/catalog/page.tsx
+++ front/src/app/(site)/catalog/page.tsx
@@ -14,21 +14,22 @@ export default async function CatalogPage() {
   const vegetables: Product[] = await getVegetablesCatalog();
   const processedFoods: Product[] = await getProcessedFoodsCatalog();
   // const productsEnvasados = await getProdComesCatalog();
-  const categories:Category[] = [
-    {name:"Vegetales de temporada", products:vegetables},
-    {name:"Frutas de temporada", products:fruits},
-    {name:"Procesados naturales", products:processedFoods},
-  ]
+  const categories: Category[] = [
+    { name: "Vegetales de temporada", products: vegetables },
+    { name: "Frutas de temporada", products: fruits },
+    { name: "Procesados naturales", products: processedFoods },
+  ];
   return (
     <>
-      <Title size="lg" border color="secondary-orange" weight="semibold" icon>Productos del Mes</Title>
-      <AccordionCatalog categories={categories}/>
-    <div className="w-full m-auto flex justify-center items-center gap-4 mt-4">
-          <RouteBtn size="lg" bgColor="primary-yellow" route="/">
-            Volver al inicio
-          </RouteBtn>
-          
-        </div>
+      <Title size="lg" border color="secondary-orange" weight="semibold" icon>
+        Productos del Mes
+      </Title>
+      <AccordionCatalog categories={categories} />
+      <div className="w-full m-auto flex justify-center items-center gap-4 mt-4">
+        <RouteBtn size="lg" bgColor="primary-yellow" route="/">
+          Volver al inicio
+        </RouteBtn>
+      </div>
     </>
   );
 }
